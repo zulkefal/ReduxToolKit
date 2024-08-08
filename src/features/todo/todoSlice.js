789d515d306b5310fed135f0c393d2df206b01b6
zulkefal <zulkefal.khan705@gmail.com>
2024-08-09 00:10:21 +0500

    c

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -28,6 +28,9 @@ export const todoSlice= createSlice({
         removeTodo:(state,action)=>{
             state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
         },
+        updateTodo:(state,action)=>{
+            state.todos = state.todos.map((todo)=> todo.id === action.payload.id ? action.payload : todo)
+        }
 
     }
 })
